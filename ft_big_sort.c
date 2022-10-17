529abbfa0c4cac350f86e03b7a7f09eec7f4beec
Zakariya Hamdouchi <zakariyahamdouchi@Zakariyas-MacBook-Pro.local>
2022-10-17 19:41:08 +0200

    “test”

--- ft_big_sort.c
+++ ft_big_sort.c
@@ -6,7 +6,7 @@
 /*   By: zakariyahamdouchi <zakariyahamdouchi@st    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 17:57:51 by zakariyaham       #+#    #+#             */
-/*   Updated: 2022/10/17 19:16:59 by zakariyaham      ###   ########.fr       */
+/*   Updated: 2022/10/17 19:36:50 by zakariyaham      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,11 +89,11 @@ void    fait_le_mouv(int elu, t_list **a, t_list **b) //utiliser (*b) et non pas
             rrr(a, b);
         if (tmp_b->cost_a > 0 && tmp_b->cost_b > 0)
             rr(a , b);
-        if (tmp_b->cost_a < 0 && tmp_b->cost_b == 0)
+        if (tmp_b->cost_a > 0 && tmp_b->cost_b == 0)
             rra(a);
         if (tmp_b->cost_b < 0 && tmp_b->cost_a == 0)
             rrb(b);
-        if (tmp_b->cost_a > 0 && tmp_b->cost_b == 0)
+        if (tmp_b->cost_a < 0 && tmp_b->cost_b == 0)
             ra(a);
         if (tmp_b->cost_b > 0 && tmp_b->cost_a == 0)
             rb(b);
